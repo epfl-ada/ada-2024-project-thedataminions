5ecf0ea6fac5025d1eeaf9a4acb33523a7f5df00
Andreas H <andreas.hygrell@campus.tu-berlin.de>
2024-11-09 16:59:06 +0100

    add fct to filter by nan entries, fct to filter comments by author

--- preprocessing_tests/data_processing.py
+++ preprocessing_tests/data_processing.py
@@ -1,27 +1,100 @@
 """Functions for processing (big) datasets. Will be called from the notebook"""
-import pandas as pd
 import time
+import pandas as pd
 
 
-def run_simple_function_on_chunks(reader, fct, print_time = False) -> pd.DataFrame: 
+def run_simple_function_on_chunks(reader, fct, print_time: bool | dict = False) -> pd.DataFrame:
     """
     Runs a given function that works on a (single) dataframe, but runs it on the given reader. 
-    The reader is an iterator object that returns the chunks, you can get it for example by 
-    calling pd.read_csv(...., chunksize=something).
     The function returns a single dataframe with the results from all chunks concatenated.
+
+    Args:
+        reader: iterator object that returns the chunks, you can get it for example by calling pd.read_csv(...., chunksize=something).
+        print_time: If False (default), does not print time data. If True, prints the average time per chunk.
+            If a tuple with two entries is given, where the fist is the chunk size used in the reader, 
+            and the second is the total number of entries in the dataset,
+            then additional data about estimated time left is printed.
     """
+    
     with reader:
+        
         result = pd.DataFrame()
         if not print_time:
             for chunk in reader:
                 result = pd.concat([result, fct(chunk)])
             return result
-        else:
+        elif print_time is True:
             time_start_global = time.time()
-            for i, chunk in reader.iterrows():
-                print(f"Going through video chunk {i}...")
+            for i, chunk in enumerate(reader):
+                print(f"Going through chunk {i}...")
                 result = pd.concat([result, fct(chunk)])
                 time_end = time.time()
                 print(f"{(time_end-time_start_global)/(i+1):.3f} secs per chunk on average.")
             return result
+        else:
+            time_start_global = time.time()
+            for i, chunk in enumerate(reader):
+                print(f"Going through chunk {i}...")
+                result = pd.concat([result, fct(chunk)])
+                time_end = time.time()
+                processed_entries = (i+1)*print_time[0]
+                entries_left = print_time[1] - processed_entries
+                avg_time_per_chunk = (time_end-time_start_global)/(i+1)
+                print(f"The first {processed_entries} entries have been processed. {entries_left} left.")
+                print(f"{avg_time_per_chunk:.3f} secs per chunk on average. Meaning  {entries_left * avg_time_per_chunk /(print_time[0]* 60):.3f} minutes left.")
+
+
+            return result
+
+
+
+def filter_comments_by_author(data: pd.DataFrame, author: int) -> pd.DataFrame:
+    """
+    Filters a given dataframe so that only entries corresponding to the given author remains.
+    
+    Args:
+        data: the dataframe to be filtered
+        author: the id of the author to be filtered by
         
+    Returns:
+        The filtered dataframe
+    """
+
+    return data.loc[data.author == author]
+
+
+def get_na_entries(data: pd.DataFrame, col: str = "any", reverse: bool = False) -> pd.DataFrame:
+    """
+    Filters a dataframe to return only rows with na elements (using pd.isna).
+    Can be specified to either return rows where any entry is na, or rows where a specified column is na, or where all columns are na.
+    
+    Args:
+        data: the dataframe to be filtered
+        col: string defining the criterion. 
+            'any' (default) returns rows where any entry is na. 
+            '<col_name>' returns rows where entries in the column <col_name> are na.
+            'all' returns rows where all entries are na.
+        reverse: if True, instead returns the complement of the filtered dataframe, i.e., 
+            all rows that do not have na entries (in any, all or a specific column, as defined by 'col')
+            
+    Returns:
+        the filtered dataframe
+    """
+
+    if col == "any":
+        if not reverse:
+            return data[data.isna().any(axis=1)]
+        else:
+            return data[data.notna().all(axis=1)]
+    elif col == "all":
+        if not reverse:
+            return data[data.isna().all(axis=1)]
+        else:
+            return data[data.notna().any(axis=1)]
+    else:
+        if not reverse:
+            return data[data.isna()[col]]
+        else:
+            return data[data.notna()[col]]
+        
+
